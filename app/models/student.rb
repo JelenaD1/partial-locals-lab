36e16cf0f0d00485ed6a67acaae860fa803fe8ef
Jelena <jelena.durovic@powerhrg.com>
2021-09-30 10:10:57 -0400

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,19 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
+require "pry"
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(user_input)
+    if user_input.empty?
+      @students = Student.all
+    else
+      @students = Student.where("name like ?", "%#{user_input}%")
+
+    end
+  end 
+
+ 
 end
